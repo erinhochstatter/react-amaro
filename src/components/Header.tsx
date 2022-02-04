a316df17d4ef7714d1b7feee8c55ee3d400c509a
Erin Hochstatter <ehochstatter@gmail.com>
2022-02-03 22:48:30 -0600

    A little more header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,27 @@
 import styles from './Header.module.scss';
+import snek from '../assets/snek.svg'
+import swagUpper from '../assets/swag_upper.svg'
+import swagLower from '../assets/swag_lower.svg'
+import sideDot from '../assets/dot_side.svg'
+
 export function Header() {
   return (
     <div data-testid="header" className={styles.container}>
-      <div className={styles.heroText}>
-        <p>Don't stop thinking about amaro</p>
+      <div className={styles.logotown}>
+        <div className={styles.logotownLeft}>
+          <img src={swagUpper} className={styles.swag} alt="a leafy boundary" />
+          <img src={sideDot} className={styles.sideDot} alt="a leafy boundary" />
+          <img src={swagLower} className={styles.swag} alt="a leafy boundary" />
+        </div>
+        <div className={styles.logotownMain}>
+          <img src={snek} className={styles.snek} alt="a chalice with a snake for the stem" />
+          <div className={styles.heroText}> Don't stop thinking about amaro </div>
+        </div>
+        <div className={styles.logotownRight}>
+          <img src={swagUpper} className={styles.reverseSwag} alt="a leafy boundary" />
+          <img src={sideDot} className={styles.sideDot} alt="a leafy boundary" />
+          <img src={swagLower} className={styles.reverseSwag} alt="a leafy boundary" />
+        </div>
       </div>
     </div>
   );
