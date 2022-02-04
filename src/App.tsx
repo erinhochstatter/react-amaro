e1e6e17301252cb0fff387ba9fce39d68739cc0a
Erin Hochstatter <ehochstatter@gmail.com>
2022-02-03 22:27:24 -0600

    Add the amaro logo elements & add them to the top

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import './App.scss'
+import styles from './App.module.scss';
 import { Header } from './components/Header'
 import {RecipeList, Recipe} from './components/Recipes'
 import { Routes, Route, Link } from "react-router-dom";
+import snek from './assets/snek.svg'
+import swagUpper from './assets/swag_upper.svg'
+import swagLower from './assets/swag_lower.svg'
+import sideDot from './assets/dot_side.svg'
 
 function App() {
   return (
-      <div className="Amaro">
+      <div className={styles.container}>
+        <div className={styles.logotown}>
+          <div className={styles.logotownLeft}>
+            <img src={swagUpper} className={styles.swag} alt="a leafy boundary" />
+            <img src={sideDot} className={styles.sideDot} alt="a leafy boundary" />
+            <img src={swagLower} className={styles.swag} alt="a leafy boundary" />
+          </div>
+          <img src={snek} className={styles.snek} alt="a chalice with a snake for the stem" />
+          <div className={styles.logotownRight}>
+            <img src={swagUpper} className={styles.reverseSwag} alt="a leafy boundary" />
+            <img src={sideDot} className={styles.sideDot} alt="a leafy boundary" />
+            <img src={swagLower} className={styles.reverseSwag} alt="a leafy boundary" />
+          </div>
+        </div>
         <Header/>
         <Routes>
           <Route path="/" element={<RecipeList />} />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import styles from './Header.module.scss';
-
 export function Header() {
   return (
     <div data-testid="header" className={styles.container}>
